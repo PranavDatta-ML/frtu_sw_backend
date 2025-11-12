f55774c425400b59261c0d9a0fe84da3c5a89cde
“abhi-etlab” <abhishek.gupta@etlab.co>
2025-11-12 15:16:13 +0530

    Changes for Cors error

--- src/app.py
+++ src/app.py
@@ -17,8 +17,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
-    allow_credentials=False,
+    allow_origins=["http://localhost:5500"],  # Allows all origins
+    allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
